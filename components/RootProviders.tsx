9f792c1df6677d2e1f434bbe41c57ebb2f1562aa
ritikaa1802 <ritika.021205@gmail.com>
2026-04-05 01:02:53 +0530

    login changes

--- components/RootProviders.tsx
+++ components/RootProviders.tsx
@@ -1,12 +1,16 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
+import { usePathname } from "next/navigation";
 import DashboardLayout from "@/components/DashboardLayout";
 
 export default function RootProviders({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  const hideAppShell = pathname === "/login" || pathname === "/signup";
+
   return (
     <SessionProvider>
-      <DashboardLayout>{children}</DashboardLayout>
+      {hideAppShell ? children : <DashboardLayout>{children}</DashboardLayout>}
     </SessionProvider>
   );
 }
